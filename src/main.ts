1ddce036f5452be259afd2df18890cbe89a2b00d
AdegbolaIsrael <88213907+AdegbolaIsrael@users.noreply.github.com>
2022-02-24 13:41:29 +0100

    i added a paragraph in the ts file and style in the scss file

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,5 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <p>just want to make a change<p>
 `
